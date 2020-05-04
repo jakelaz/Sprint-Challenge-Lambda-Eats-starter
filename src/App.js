3764474759dc4db844f34efec96fc395aa775af7
jakelaz <jake@openmessage.io>
2020-05-03 23:42:30 -0400

    completed MVP objectives

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+
+import PizzaForm from './Components/PizzaForm';
+
+import { Nav, NavItem, NavLink } from 'reactstrap';
+
 
 const App = () => {
+  const [pizzas, setPizzas] = useState([]);
+  
+  const handleSetNewPizza = (newPizza) =>{
+    console.log("newPizza",newPizza)
+    setPizzas([...pizzas, newPizza])
+ 
+  }
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="appWrapper">
+      
+      <Nav>
+        <NavItem>
+          <NavLink href="/">Lambda Eats</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/pizza">Order Pizza</NavLink>
+        </NavItem>
+      </Nav>  
+      <Route path="/pizza">
+        <PizzaForm handleSetNewPizza={handleSetNewPizza} />
+        <b id="orderData">{JSON.stringify(pizzas)}</b>
+      </Route>
+      <Route exact path="/">
+        <h1>Welcome to Lambda Eats</h1>
+      </Route>      
+    </div>
   );
 };
 export default App;
